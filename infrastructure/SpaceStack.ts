ae50f48caa0f548125505df911564be0d8cdbc8e
Joe Cui <xiaozhou.cui@gmail.com>
2021-12-28 11:54:02 +1000

    Add console log in CloudWatch log group

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -14,22 +14,24 @@ export class SpaceStack extends Stack {
   // app is of type Construct
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props)
-    // "this" is the context/scope of type Construct
-    const helloLambda = new LambdaFunction(this, 'helloLambda', {
-      runtime: Runtime.NODEJS_14_X,
-      // code can come from a file, a docker build, S3 bucket etc.
-      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-      // map the handler to the exported "main" property of hello.js
-      handler: 'hello.main'
-    })
 
+    // lambda written pure js (not typescript)
+    // const helloLambda = new LambdaFunction(this, 'helloLambda', {
+    //   runtime: Runtime.NODEJS_14_X,
+    //   // code can come from a file, a docker build, S3 bucket etc.
+    //   code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+    //   // map the handler to the exported "main" property of hello.js
+    //   handler: 'hello.main'
+    // })
+
+    // "this" is the context/scope of type Construct
     const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
       entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
       handler: 'handler' // exported handler property from hello.ts
     })
 
     // integrate lambda with API Gateway
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda)
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs)
     // provide api gateway as resource
     const helloLambdaResource = this.api.root.addResource('hello')
     helloLambdaResource.addMethod('GET', helloLambdaIntegration)
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,6 +1,8 @@
 import { v4 } from "uuid";
 
 async function handler (event: any, context: any) {
+  console.log('Got an event:')
+  console.log(event)
   return {
     statusCode: 200,
     body: 'Hello from lambda!' + v4()
