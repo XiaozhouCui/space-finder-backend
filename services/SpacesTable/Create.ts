f826b84b33313360f0dcc72b51ddf1ae82370d4f
Joe Cui <xiaozhou.cui@gmail.com>
2021-12-29 12:42:23 +1000

    Create new item in DynamoDB from local machine without deploy

---
+++ services/SpacesTable/Create.ts
@@ -0,0 +1,30 @@
+import { DynamoDB } from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
+import { v4 } from 'uuid'
+
+const dbClient = new DynamoDB.DocumentClient()
+
+// APIGatewayProxyEvent: the event which will be received by lambda when APIGateway call it
+async function handler (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+  const result: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: 'Hello from DynamoDB.'
+  }
+
+  const item = {
+    spaceId: v4()
+  }
+
+  try {
+    await dbClient.put({
+      TableName: 'SpacesTable',
+      Item: item
+    }).promise()
+  } catch (error: any) {
+    result.body = error.message
+  }
+
+  return result
+}
+
+export {handler}
